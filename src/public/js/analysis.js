58142ada3e2115982a2546cd74da6b783006539f
Barrior <barrior@qq.com>
2017-06-05 13:25:13 +0800

    adjust analysis

--- src/public/js/analysis.js
+++ src/public/js/analysis.js
@@ -8,15 +8,23 @@ export default function analysis() {
         cnzz.src = 'https://s19.cnzz.com/z_stat.php?id=1262083162';
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(cnzz, s);
+
+        window.addEventListener('hashchange', () => {
+            _czc.push(["_trackPageview", location.hash.substring(1)]);
+        });
     })();
 
-    // 百度统计
+    // Baidu analysis
     (function () {
         window._hmt = window._hmt || [];
         var hm = document.createElement("script");
         hm.src = "https://hm.baidu.com/hm.js?8e8c9172687affd39edbb31c89ebbf97";
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(hm, s);
+
+        window.addEventListener('hashchange', () => {
+            window._hmt.push(['_trackPageview', location.hash.substring(1)]);
+        });
     })();
 
     // Growing IO
@@ -24,6 +32,8 @@ export default function analysis() {
         var _vds = _vds || [];
         window._vds = _vds;
         _vds.push(['setAccountId', 'b8678e52a8af5d0f']);
+        _vds.push(['trackBot', false]);
+        _vds.push(['enableHT', true]);
         (function () {
             var vds = document.createElement('script');
             vds.type = 'text/javascript';
