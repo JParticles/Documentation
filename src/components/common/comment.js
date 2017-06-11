7b80fc3dd52845fdcbded6f77a44c9db3ee1b0cf
barrior <barrior@qq.com>
2017-06-11 15:53:07 +0800

    add DISQUS comment

--- src/components/common/comment.js
+++ src/components/common/comment.js
@@ -1,20 +1,33 @@
 import React, {Component} from 'react';
+import {parseRouter} from 'js/parse_router';
 
 export default class Comment extends Component {
-
-    constructor() {
-        super();
-    }
-
     componentDidMount() {
-
+        const router = parseRouter();
+        window.disqus_config = function () {
+            // Replace PAGE_URL with your page's canonical URL variable
+            this.page.url = 'https://jparticles.js.org' + router.fullPath;
+            // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            this.page.identifier = router.fullPath;
+        };
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://jparticles.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
     }
 
     render() {
-        return null;
         return (
             <div className="comment">
-                Comment
+                <div id="disqus_thread"></div>
+                <noscript>
+                    Please enable JavaScript to view the
+                    <a href="https://disqus.com/?ref_noscript">
+                        comments powered by Disqus.
+                    </a>
+                </noscript>
             </div>
         )
     }
