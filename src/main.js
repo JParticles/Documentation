8cae5026b633e9e0fbf3adcf816633b3d5575d87
Barrior <barrior@qq.com>
2019-03-12 20:12:00 +0800

    Feat: entry files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store from "./store/index";
+import '@/styles/build.scss';
 
 Vue.config.productionTip = false;
 
