9c0dd22ed31579fde2d7c69c41569e88b93e2454
Barrior <barrior@qq.com>
2019-03-19 19:03:58 +0800

    Chores: improve loading.js

--- public/loading.js
+++ public/loading.js
@@ -1,21 +1,76 @@
 ;(function() {
-  function find() {}
-
-  window.globalConfig = {
+  const globalConfig = (window.globalConfig = {
     themeColor: '#00be70',
-    languages: [],
-    langCode: 'en',
+    language: {},
     isMobile() {
       return document.documentElement.clientWidth <= 768
     },
-    setLang(langCode) {
-      this.langCode = langCode
-      const lang = find(this.languages, langCode)
-      document.title = lang.title
-      document.getElementsByTagName('html')[0].lang =
-        languageField == 'cn' ? 'zh-cn' : languageField
+    getLanguageConfig() {
+      let userLanguage = location.hash.replace(/#\/([\w-]*?)\/.*/i, '$1')
+      if (userLanguage === 'zh-cn') {
+        userLanguage = 'cn'
+      }
+
+      if (!languages[userLanguage]) {
+        userLanguage = navigator.language && navigator.language.toLowerCase()
+        if (userLanguage === 'zh-cn') userLanguage = 'cn'
+      }
+
+      if (!languages[userLanguage]) {
+        userLanguage = 'en'
+      }
+
+      return (globalConfig.language = languages[userLanguage])
     },
+    setDocumentLanguage() {
+      const language = globalConfig.language
+      document.title = language.title
+      document.getElementsByTagName('html')[0].lang =
+        language.languageCodeOnHtml
+    },
+  })
+
+  globalConfig.getLanguageConfig()
+  globalConfig.setDocumentLanguage()
+
+  const $body = document.getElementsByTagName('body')[0]
+  const $layer = document.getElementById('loading-layer')
+  const $container = document.getElementById('loading-container')
+  const progressText = globalConfig.language.progressText
+  const loading = new window.JParticles.waveLoading($container, {
+    font: 'normal 900 13px Arial',
+    color: globalConfig.themeColor,
+    fillColor: globalConfig.themeColor,
+    duration: 5000,
+    resize: false,
+  })
+
+  loading
+    .onProgress(progress => {
+      // set white text when the progress more than 60%
+      if (progress >= 60) {
+        loading.setOptions({
+          color: '#fff',
+        })
+      }
+      return progressText + Math.ceil(progress) + '%'
+    })
+    .onFinished(() => {
+      $body.classList.remove('overflow-hidden')
+      $layer.classList.add('zoom-out')
+    })
+
+  function animationendHandler() {
+    $layer.removeEventListener('animationend', animationendHandler)
+    $body.removeChild($layer)
   }
 
-  console.log('sdf')
+  $layer.addEventListener('animationend', animationendHandler)
+
+  function loadingDone() {
+    window.removeEventListener('load', loadingDone)
+    loading.done()
+  }
+
+  window.addEventListener('load', loadingDone)
 })()
