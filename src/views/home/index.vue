3cd4f8a5771ad6d0e1b79fd0fa929efd117a810f
Barrior <barrior@qq.com>
2019-05-04 18:23:50 +0800

    Feat: save workspace

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,13 +5,13 @@
       <div class="title">JParticles</div>
       <div class="description">
         {{ homeData.description }}
-        <x-link class="readmore" to="/examples/intro">
+        <x-link class="see-more" to="/examples/intro">
           {{ homeData.seeMore }}
         </x-link>
       </div>
-      <div class="quick-start">
-        <x-link class="btn btn-success pr" to="/examples/quick_start">
-          <span class="pa">{{ homeData.quickStart }}</span>
+      <div class="btn-box">
+        <x-link class="btn btn-success" to="/examples/quick_start">
+          <span class="text">{{ homeData.quickStart }}</span>
         </x-link>
       </div>
     </div>
@@ -32,11 +32,15 @@ export default {
     createEffect() {
       const settings = this.isSmallScreen
         ? {
+            opacity: 0.3,
             proximity: 50,
             num: 0.25,
             maxSpeed: 0.7,
           }
-        : { proximity: 100 }
+        : {
+            opacity: 0.5,
+            proximity: 100,
+          }
       new JParticles.particle(
         this.$refs.bg,
         Object.assign(
@@ -65,12 +69,73 @@ export default {
     @include absolute-full();
   }
   .content {
+    color: $green;
+    text-align: center;
     position: absolute;
     z-index: 2;
     left: 0;
     right: 0;
     top: 50%;
     transform: translateY(-50%);
+  }
+  .title {
+    font-size: rem(60);
+    font-weight: 900;
+  }
+  .description {
+    margin-bottom: rem(50);
+    padding: 0 rem(50);
+    font-size: rem(18);
+  }
+  .see-more {
+    display: inline-block;
+    line-height: 1.2;
+    color: $green;
+    border-bottom: 1px solid $green;
+    font-size: 65%;
+    transform: translateY(rem(-2));
+    &:hover {
+      color: $green-hover;
+      border-bottom-color: $green-hover;
+    }
+  }
+  .btn-box {
+    .btn {
+      $btn-height: rem(50);
+      height: $btn-height;
+      padding: 0 rem(20);
+      border-color: $green;
+      border-radius: rem(4);
+      box-shadow: none;
+      background-color: transparent;
+      font-size: rem(18);
+      color: $green;
+      position: relative;
+      overflow: hidden;
+      &:after {
+        content: '';
+        background-color: $green;
+        position: absolute;
+        top: 50%;
+        bottom: 50%;
+        left: rem(-10);
+        right: rem(-10);
+        transition: 0.4s ease-out;
+        transform: rotate(45deg);
+      }
+      .text {
+        line-height: $btn-height;
+        position: relative;
+        z-index: 3;
+      }
+      &:hover {
+        color: #fff;
+        &:after {
+          top: -100%;
+          bottom: -100%;
+        }
+      }
+    }
   }
 }
 </style>
