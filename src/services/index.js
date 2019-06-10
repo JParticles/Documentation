fa2c8a8d76d8c4b25a7f5aaf942a0aa97e15a000
Barrior <barrior@qq.com>
2019-06-10 22:19:13 +0800

    refactor: services

--- src/services/index.js
+++ src/services/index.js
@@ -1,58 +1,15 @@
 import apisauce from 'apisauce'
-import { getDocPath } from '@/utils/path'
-
-const { isPlainObject } = JParticles.utils
+import transforms from './transforms'
 
 const API = () => {
   const api = apisauce.create({
     timeout: 6000,
   })
 
-  api.addResponseTransform(response => {
-    // 统一错误提示信息处理
-    if (!response.ok) {
-      let errorMsg = '未知错误'
-      if (response.status !== null) {
-        if (isPlainObject(response.data) && response.data.message) {
-          errorMsg = response.data.message
-        }
-        switch (response.status) {
-          case 401:
-            errorMsg = '请重新登录'
-            setTimeout(() => {
-              window.location.href = '/'
-            }, 1000)
-            break
-          case 403:
-            errorMsg = '您无权浏览该页面'
-            setTimeout(() => {
-              window.location.href = '/'
-            }, 1000)
-            break
-          case 429:
-            errorMsg = '操作太频繁了'
-            break
-        }
-        if (response.status >= 500) {
-          errorMsg = '服务器错误，请稍后再试'
-        }
-      } else {
-        switch (response.problem) {
-          case 'TIMEOUT_ERROR':
-            errorMsg = '服务器响应超时，请稍后再试'
-            break
-          case 'CANCEL_ERROR':
-            errorMsg = '取消发送请求'
-            break
-        }
-      }
-      let originData = isPlainObject(response.data) ? response.data : {}
-      response.data = Object.assign(originData, { message: errorMsg })
-    }
-  })
+  transforms(api)
 
   return {
-    introDoc: () => api.get(getDocPath('intro')),
+    $instance: api,
   }
 }
 
